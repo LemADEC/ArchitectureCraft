c77e5333a309c7d3403020602995bbc5f65e65ad
Sporkley <spork@llamaherder.com>
2016-05-20 10:20:29 -0700

    fixes #3

--- src/mod/gcewing/architecture/ShapeBlock.java
+++ src/mod/gcewing/architecture/ShapeBlock.java
@@ -173,7 +173,7 @@ public class ShapeBlock extends BaseBlock<ShapeTE> {
 			//System.out.printf("ShapeBlock.harvestBlock: spawning %s\n", stack);
 			spawnAsEntity(world, pos, stack);
 			if (ste.secondaryBlockState != null) {
-				stack = BaseUtils.blockStackWithState(ste.secondaryBlockState, 1);
+				stack = ArchitectureCraft.itemCladding.newStack(ste.secondaryBlockState, 1);
 				//System.out.printf("ShapeBlock.harvestBlock: spawning %s\n", stack);
 				spawnAsEntity(world, pos, stack);
 			}
